e7e76b3e9e9d22f5863c21ea3344fe1c31604f84
bnorum <bnorum1@pride.hofstra.edu>
2023-02-19 18:39:43 -0500

    so so many changes
    -added js
    -added cool fullscreen images in js
    -added faded in (may remove so ugly)
    -add herobrine

---
+++ script.js
@@ -0,0 +1,24 @@
+function expandImage(img) {
+    var overlay = document.createElement("div");
+    overlay.className = "fullscreen";
+    overlay.onclick = function() {
+      document.body.removeChild(overlay);
+    };
+    var fullImg = document.createElement("img");
+    fullImg.src = img.src;
+    overlay.appendChild(fullImg);
+    document.body.appendChild(overlay);
+  }
+
+function fadeInBody() {
+  var opacity = 0;
+  var timer = setInterval(function() {
+    if (opacity >= 1) {
+        clearInterval(timer);
+      }
+      document.body.style.opacity = opacity;
+      opacity += 0.5;
+    }, 50);
+  }
+  
+  window.onload = fadeInBody;
