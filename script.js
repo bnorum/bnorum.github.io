35f147131f1548d128332f004bfbb118d6b53dbf
Brady Norum <bnorum1@pride.hofstra.edu>
2023-04-18 17:17:42 -0400

    added pure css modals

--- script.js
+++ script.js
@@ -1,26 +1,8 @@
-function expandImage(img) {
-    var overlay = document.createElement("div");
-    overlay.className = "fullscreen";
-    overlay.onclick = function() {
-      document.body.removeChild(overlay);
-    };
-    var fullImg = document.createElement("img");
-    fullImg.src = img.src;
-    overlay.appendChild(fullImg);
-    
-    var altText = document.createElement("p");
-    altText.textContent = img.alt;
-    overlay.appendChild(altText);
-    fullImg.style.display = "block";
-    fullImg.style.margin = "auto";
-    altText.style.textAlign = "center";
-    altText.style.color = "#B97375";
-    altText.style.fontSize = "large";
 
-
-    document.body.appendChild(overlay);
+function onModalClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
   }
-
 
 function fadeInBody() {
   var opacity = 0;
