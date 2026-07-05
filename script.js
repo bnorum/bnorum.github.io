06f797f45a4f41e3cdd5b9e2cc220ac45e4c91fc
Brady Norum <bnorum1@pride.hofstra.edu>
2026-07-05 19:07:38 -0400

    oops AGAIN!

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-import * as THREE from "three";
-import { OBJLoader } from "three/addons/loaders/OBJLoader.js";
+import * as THREE from "https://unpkg.com/three@0.161.0/build/three.module.js";
+import { OBJLoader } from "https://unpkg.com/three@0.161.0/examples/jsm/loaders/OBJLoader.js";
 
 const year = document.getElementById('year');
 if (year) {
